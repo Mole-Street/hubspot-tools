57418b41dc4a58f226afd57f9d38ffd30a5116ec
Chris Stone <chris@cstone.io>
2023-04-28 16:03:52 -0400

    testing field only

--- packages/hubspot-laravel-mix/index.js
+++
@@ -1,8 +0,0 @@
-// Example: require('@igomoon/hubspot-laravel-mix')(mix)
-
-module.exports = (mix = null) => { 
-	if (!!mix) {
-		const MixHubspot = require('./MixHubspot')
-		mix.extend('hubspot', new MixHubspot())
-	}
-}
