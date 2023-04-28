ebca1db216eb070a147b015d7ab9aef47148417f
Chris Stone <chris@cstone.io>
2023-04-28 14:39:53 -0400

    Adds Style class and working example

--- packages/hubspot-fields-js/index.js
+++ packages/hubspot-fields-js/index.js
@@ -1,4 +1,5 @@
 const Group = require('./src/fields/Group');
+const Style = require('./src/fields/Style');
 const Field = require('./src/fields/Field');
 const FieldsPlugin = require('./src/plugins/FieldsPlugin');
 const { randomId, parseArgs } = require('./src/Helpers');
@@ -6,6 +7,7 @@ const { randomId, parseArgs } = require('./src/Helpers');
 module.exports = {
     // Field + Group helpers
     Group,
+    Style,
     Field,
     // Webpack plugin
     FieldsPlugin,
